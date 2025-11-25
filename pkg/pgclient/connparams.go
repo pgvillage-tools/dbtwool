6e8315e2fb4b7ae799b48e8343408fb261f71718
Robert Griffioen <robertgriffioen@github.com>
2025-11-25 15:31:37 +0100

    Added connectionparams for pg

---
+++ pkg/pgclient/connparams.go
@@ -0,0 +1,41 @@
+package pgclient
+
+import (
+	"fmt"
+
+	"github.com/pgvillage-tools/dbtwool/pkg/utils"
+)
+
+// ConnParams objects define connection parameters for a DB2 connection
+type ConnParams struct {
+	Host     string
+	Port     string
+	Database string
+	User     string
+	Password string
+	SslMode  string
+}
+
+func (cp ConnParams) GetConnectionString() string {
+	return fmt.Sprintf(
+		"host=%s port=%s dbname=%s user=%s password=%s sslmode=%s",
+		cp.Host,
+		cp.Port,
+		cp.Database,
+		cp.User,
+		cp.Password,
+		cp.SslMode,
+	)
+}
+
+// ConnParamsFromEnv generates a new default ConnParams from env variables with defaults
+func ConnParamsFromEnv() ConnParams {
+	return ConnParams{
+		Host:     utils.GetEnv("PGHOST", "locahost"),
+		Port:     utils.GetEnv("PGPORT", "5432"),
+		Database: utils.GetEnv("PGDATABASE", "postgres"),
+		User:     utils.GetEnv("PGUSER", "postgres"),
+		Password: utils.GetEnv("PGPASSWORD", "postgres"),
+		SslMode:  "disable",
+	}
+}
