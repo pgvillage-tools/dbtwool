7154f9701e406893c2c570c1cbf79f22ccf17912
Robert Griffioen <robertgriffioen@github.com>
2026-01-31 15:15:38 +0100

    Implemented stage command.

---
+++ pkg/ruperformance/db2_helper.go
@@ -0,0 +1,45 @@
+// Package lobperformance handles all work regarding LOB performance tests
+package ruperformance
+
+import (
+	"fmt"
+)
+
+// DB2Helper is a helper to return queries for a specific RDBMS type
+type DB2Helper struct {
+	schemaName string
+	tableName  string
+}
+
+// CreateSchemaSQL returns an RDBMS specific query to create a schema
+func (helper DB2Helper) CreateSchemaSQL() string {
+	sql := fmt.Sprintf(`
+CREATE SCHEMA %v;
+`, helper.schemaName)
+
+	logger.Debug().Msg(sql)
+	return sql
+}
+
+// CreateTableSQL returns a CREATE TABLE query for DB2
+func (helper DB2Helper) CreateTableSQL() string {
+	sql := fmt.Sprintf(`
+CREATE TABLE %s.%s (
+    acct_id     INTEGER NOT NULL,
+    txn_ts      TIMESTAMP NOT NULL,
+    amount      DECIMAL(12,2) NOT NULL,
+    descr      CHAR(100) NOT NULL
+)
+ORGANIZE BY ROW;`, helper.schemaName, helper.tableName)
+	logger.Debug().Msg(sql)
+	return sql
+}
+
+// CreateTableSQL returns a CREATE TABLE query for DB2
+func (helper DB2Helper) CreateIndexSQL() string {
+	sql := fmt.Sprintf(`
+CREATE INDEX index_account_transaction_acct
+    ON %v.%v (acct_id, txn_ts);`, helper.schemaName, helper.tableName)
+	logger.Debug().Msg(sql)
+	return sql
+}
---
+++ pkg/ruperformance/db_helper.go
@@ -0,0 +1,8 @@
+package ruperformance
+
+// DBHelper is an interface to help returning queries for a specific RDBMS type
+type DBHelper interface {
+	CreateSchemaSQL() string
+	CreateTableSQL() string
+	CreateIndexSQL() string
+}
